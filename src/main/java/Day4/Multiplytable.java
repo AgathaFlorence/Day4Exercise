f6811eef1b2acd76770d2f5d8db9aa6cb1c17804
AgathaFlorence <agathaflorencetjhia@outlook.com>
2024-09-05 16:53:38 +0700

    Fix multiplication

--- src/main/java/Day4/Multiplytable.java
+++ src/main/java/Day4/Multiplytable.java
@@ -3,19 +3,18 @@ import java.util.Scanner;
 
 public class Multiplytable {
     public static void main (String[] args){
+
+        System.out.print("Please input the mupltiplication table number: ");
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
 
+
         int a;
         int b;
-        for(a = 1; a <=n; ++a) {
-            for (b = 1; b <=n; ++b) ;
-            {
-                System.out.printf(a+" ");
-                System.out.print((a+a)+" ");
-                System.out.print((a+a+a)+" ");
-                System.out.print((a+a+a+a)+" ");
-                System.out.print((a+a+a+a+a)+" ");
+        for(a = 1; a <=n; a++) {
+            for (b = 1; b <=n; b++){
+                System.out.printf(a*b+" ");
+
             }
             System.out.println();
         }
